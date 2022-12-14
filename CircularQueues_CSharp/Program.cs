e0cce638a7051183a8be425cab219f5d40d77775
sarahannata <sarahannata@gmail.com>
2022-12-14 16:40:20 +0700

    Membuat class Queues

--- CircularQueues_CSharp/Program.cs
+++ CircularQueues_CSharp/Program.cs
@@ -2,5 +2,15 @@
 
 namespace CircularQueues_CSharp
 {
-
+    class Queues
+    {
+        int FRONT, REAR, max = 5;
+        int[] queue_array = new int[5];
+        public Queues()
+        {
+            /*Initializing the values of the variables REAR and FRONT to -1 to indicate that the queue is initially empty.*/
+            FRONT = -1;
+            REAR = -1;
+        }
+    }
 }
