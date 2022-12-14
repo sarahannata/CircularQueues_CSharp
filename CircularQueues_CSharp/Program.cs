07b816f33d89365a1e9e0bf8a8a70cb753ee7bed
sarahannata <sarahannata@gmail.com>
2022-12-14 16:56:56 +0700

    Membuat public void remove

--- CircularQueues_CSharp/Program.cs
+++ CircularQueues_CSharp/Program.cs
@@ -20,7 +20,47 @@ namespace CircularQueues_CSharp
                 Console.WriteLine("\nQueue overflow\n");
                 return;
             }
-
+            /* The following statement checks whether the queue is empty. If the queue is empty, then the value of the REAR and FRONT variables is set to 0.*/
+            if(FRONT == -1)
+            {
+                FRONT = 0;
+                REAR = 0;
+            }
+            else 
+            {
+                /* If the REAR is at the last position of the array, than the value of REAR is set to 0 that corresponds to the first position of the array. */
+                if (REAR == max -1) REAR = 0;
+                else
+                    /* If REAR is not at the last position, then its value is incremented by one. */
+                    REAR = REAR + 1;    
+            }
+            /* Once the posiiton of REAR is determined, the element is added at its proper place. */
+            queue_array[REAR] = element;
+        }
+        public void remove()
+        {
+            /*Checks wheter the queue is empty.*/
+            if (FRONT == -1)
+            {
+                Console.WriteLine("Queue underflow\n");
+                return;
+            }
+            Console.WriteLine("\nThe element deleted from the queue is: " + queue_array[FRONT] + "\n");
+            /*Checks if the queue has one element. */
+            if (FRONT == REAR)
+            {
+                FRONT = -1;
+                REAR = -1;
+            }
+            else
+            {
+                /* If the element to be deleted is at the last position of the array, then the value of FRONT is set to 0 i.e to the first element of the array. */
+                if (FRONT == max - 1)
+                    FRONT = 0;
+                else
+                    /* FRONT is incremented by one if it is not the first element of array. */
+                    FRONT = FRONT + 1;
+            }
         }
     }
 }
