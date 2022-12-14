0f060764de1ad59387ce8a286bfbc0aaa9053158
sarahannata <sarahannata@gmail.com>
2022-12-14 16:44:53 +0700

    Membuat if untuk check overflow condition

--- CircularQueues_CSharp/Program.cs
+++ CircularQueues_CSharp/Program.cs
@@ -12,5 +12,15 @@ namespace CircularQueues_CSharp
             FRONT = -1;
             REAR = -1;
         }
+        public void insert(int element)
+        {
+            /*This statement checks for the overflow condition. */
+            if ((FRONT == 0 && REAR == max - 1) || (FRONT == REAR + 1))
+            {
+                Console.WriteLine("\nQueue overflow\n");
+                return;
+            }
+
+        }
     }
 }
